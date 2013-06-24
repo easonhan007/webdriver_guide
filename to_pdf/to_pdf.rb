c34d709ec454a7a3c35bb61e5474a266ac43c421
easonhan <nbkhic@qq.com>
2013-06-24 16:15:24 +0800

    modify style

--- to_pdf/to_pdf.rb
+++ to_pdf/to_pdf.rb
@@ -1,11 +1,15 @@
+#encoding: utf-8
 require 'pdfkit'
 require 'redcarpet'
 
 PDFKit.configure do |config|
   config.wkhtmltopdf = 'D:\wkhtmltopdf\wkhtmltopdf.exe'
   config.default_options = {
-    :page_size => 'Letter',
-    :print_media_type => true
+    # :page_size => 'Letter',
+    :print_media_type => true,
+	:no_background => true,
+	:encoding => 'utf-8',
+	
   }
   # config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server.
 end
@@ -13,15 +17,32 @@ end
 md = File.join('..', '**', '*.md')
 md_files = Dir.glob(md)
 
-md_files.each do |md|
-	File.open('all_in_one.md', 'a+') do |handle|
-		File.open(md, 'r') {|f| handle.write(f.read) }
+all_in_one = 'all_in_one.md'
+md_files.each_with_index do |md, index|
+	File.open(all_in_one, 'a+') do |handle|
+		File.open(md, 'r') do |f|
+			handle.write('-' * 20) if index != 0
+			20.times { handle.puts '' } if index != 0
+			handle.write(f.read) 
+		end #File.open
 	end
 end
+all_in_one_html = 'all_in_one.html'
+File.open(all_in_one_html, 'w') do 
+	|f| f.write '<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title>Book</title>		
+		<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" />		
+		<style>body {margin: 1.5em; font: 20px/40px 微软雅黑}</style>
+	</head>
+	<body>'
+end 
+system("mdt #{all_in_one} >> #{all_in_one_html}")
+File.open(all_in_one_html, 'a+') { |f| f.write'</body></html>'}
 
-html = File.open('all_in_one.html', 'r') { |f| f.read }
+html = File.open(all_in_one_html, 'r') { |f| f.read }
 kit = PDFKit.new(html)
-kit.stylesheets << 'bootstrap.min.css'
-kit.stylesheets << 'bootstrap-responsive.min.css'
-
 kit.to_file('book.pdf')
+File.delete(all_in_one)
+File.delete(all_in_one_html)
